5008d7a9a2f5cb574c5fd7057f63a91bd7b25bfb
bbm <brian.maranville@nist.gov>
2024-06-18 15:17:44 -0400

    simplify build - just load all wheel files from manifest

--- build_standalone.sh
+++ build_standalone.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
 HERE=$(pwd)
-BUMPS_DIR=/Users/bbm/dev/bumps
-REFL1D_DIR=/Users/bbm/dev/refl1d
-MOLGROUPS_DIR=/Users/bbm/dev/molgroups
+BUMPS_DIR=$HOME/dev/bumps
+REFL1D_DIR=$HOME/dev/refl1d
+MOLGROUPS_DIR=$HOME/dev/molgroups
 TARGET=$HERE/public/wheels
 export BUILD_EXTENSION=True
 
-cd $BUMPS_DIR && python setup.py bdist_wheel
-export BUMPS_WHEEL_FILE=$(ls dist)
+cd $BUMPS_DIR && pyodide build
 #cd $REFL1D_DIR && python setup.py bdist_wheel
 cd $REFL1D_DIR && pyodide build
-export REFL1D_WHEEL_FILE=$(ls dist)
 cd $MOLGROUPS_DIR && python setup.py bdist_wheel
-export MOLGROUPS_WHEEL_FILE=$(ls dist)
 
 cd $HERE
-npm run build -- --sourcemap
 mkdir -p $TARGET
 cp $BUMPS_DIR/dist/*.whl $TARGET
 cp $REFL1D_DIR/dist/*.whl $TARGET
 cp $MOLGROUPS_DIR/dist/*.whl $TARGET
+
+npm run build -- --sourcemap
