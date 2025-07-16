bbdbf13d90abed6b4b9478e07578d29c2cb46556
bbm <brian.maranville@nist.gov>
2025-07-16 09:26:05 -0400

    use local submodules for bumps refl1d molgroups

--- build_standalone.sh
+++ build_standalone.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 HERE=$(pwd)
-BUMPS_DIR=$HOME/dev/bumps
-REFL1D_DIR=$HOME/dev/refl1d
-MOLGROUPS_DIR=$HOME/dev/molgroups
+BUMPS_DIR=$HERE/bumps
+REFL1D_DIR=$HERE/refl1d
+MOLGROUPS_DIR=$HERE/molgroups
 TARGET=$HERE/public/wheels
 export BUILD_EXTENSION=True
 
-cd $BUMPS_DIR && pyodide build
+cd $BUMPS_DIR && rm -rf dist && pyodide build
 #cd $REFL1D_DIR && python setup.py bdist_wheel
-cd $REFL1D_DIR && pyodide build
-cd $MOLGROUPS_DIR && python setup.py bdist_wheel
+cd $REFL1D_DIR && rm -rf dist && pyodide build
+cd $MOLGROUPS_DIR && rm -rf dist && pyodide build
 
 cd $HERE
 mkdir -p $TARGET
+rm $TARGET/*.whl
 cp $BUMPS_DIR/dist/*.whl $TARGET
 cp $REFL1D_DIR/dist/*.whl $TARGET
 cp $MOLGROUPS_DIR/dist/*.whl $TARGET
