bef4b52f95222a7d0ca7796c3f3e53e340b6417c
bbm <brian.maranville@nist.gov>
2025-07-16 09:25:19 -0400

    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
-import { join } from 'node:path'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import svgLoader from "vite-svg-loader";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default ({mode}) => {
-  return defineConfig({
-    plugins: [
-      vue(),
-    ],
-    base: '',
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-        './asyncSocket.ts': join(process.cwd(), 'src', 'asyncWorkerSocket.ts'),
-        'socket.io-client': join(process.cwd(), 'src', 'asyncWorkerSocket.ts'),
-      }
+export default defineConfig({
+  plugins: [vue(), svgLoader()],
+  base: "",
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+  worker: {
+    format: 'es',
+    rollupOptions: {
+      external: ["node-fetch"],
     },
-    define: {
-      // By default, Vite doesn't include shims for NodeJS/
-      // necessary for segment analytics lib to work
-      global: {},
-      APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    },
-    worker: {
-      format: 'es',
-      rollupOptions: {
-        external: ["node-fetch"],
-      },
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          // Default
-          // dir: (mode == 'standalone') ? 'dist_standalone': join('dist', process.env.npm_package_version),
-          entryFileNames: (mode == 'production') ? 'assets/[name].js' : 'assets/[name].[hash].js',
-          assetFileNames: (mode == 'production') ? 'assets/[name][extname]' : 'assets/[name].[hash][extname]',
-          // chunkFileNames: "chunk-[name].js",
-          // manualChunks: undefined,
-        }
-      }
-    }
-  })
-}
+  },
+});
